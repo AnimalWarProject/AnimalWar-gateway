87222bc806d37c89c0c6297b5312cf4e8fe431d2
JUNGI JEONG <junejjk@gmail.com>
2023-10-17 12:42:55 +0900

    init

---
+++ src/main/java/com/example/animalwargateway/AnimalwargatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalwargateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalwargatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalwargatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/animalwargateway/AnimalwargatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalwargateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalwargatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
